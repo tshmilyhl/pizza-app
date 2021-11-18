1d80d01f3402bc58a8177e997ebb4df9b0a40943
tshmilyhl <985154610@qq.com>
2021-11-18 20:36:37 +0800

    init project & complete nav component

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+ <div class="container">
+   <app-header></app-header>
+ </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+export default {
+  name: 'app',
+  components:{
+    appHeader:Header
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="py-2" href="#" aria-label="Product">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+             fill="none" stroke="currentColor" stroke-linecap="round"
+             stroke-linejoin="round" stroke-width="2" class="d-block mx-auto"
+             role="img" viewBox="0 0 24 24" focusable="false">
+          <title>Product</title>
+          <circle cx="12" cy="12" r="10"/>
+          <path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95
+           6.06M14.31 16H2.83m13.79-4l-5.74 9.94"/></svg>
+      </a>
+      <a href="/" class="navbar-brand">Pizza点餐系统</a>
+      <ul class="navbar-nav">
+        <li><a href="#" class="nav-link">主页</a></li>
+        <li><a href="#" class="nav-link">菜单</a></li>
+        <li><a href="#" class="nav-link">管理</a></li>
+        <li><a href="#" class="nav-link">关于我们</a></li>
+      </ul>
+
+      <ul class="navbar-nav ml-auto">
+        <li><a href="#" class="nav-link">登录</a></li>
+        <li><a href="#" class="nav-link">注册</a></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
